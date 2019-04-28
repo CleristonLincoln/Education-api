2cf9e79096c5bd771ebdcacf8a9617a5ebb08575
Cleriston Lincoln <cleristonlincoln@hotmail.com>
2019-04-28 09:33:20 -0300

    select attributes get students
    retornando somente os atributos id e name em getStudents

---
+++ src/main/java/com/education/repository/projection/StudentProjectionShort.java
@@ -0,0 +1,12 @@
+package com.education.repository.projection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class StudentProjectionShort {
+
+		private Long id;
+		private String name;
+}
